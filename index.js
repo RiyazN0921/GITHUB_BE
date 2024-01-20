b5555e9bf2e9fcb783c9d3c87e8a12044fe81304
RiyazN0921 <riyazn886@gmail.com>
2024-01-20 17:11:24 +0530

    home api added

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ const port = process.env.PORT
 app.use(bodyParser.json())
 app.use(cors())
 
+app.get('/', (req, res) => {
+  console.log('welcome to backend!')
+})
 app.use('/api/github', require('./route/repo'))
 
 app.listen(port, async () => {
